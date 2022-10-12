79005e8661476ad7239aa0c0e4821e44514fdb00
ChoiYuBin <choi82354@gmail.com>
2022-10-12 09:04:59 +0900

    Fix Meat Script, Fix Mouse Script
    Meat스크립트에 고기의 익은 정도를 알 수 있는 변수를 만들었고, Mouse로 Meat 오브젝트와 상호작용 할 수 있는 스크립트를 작성하였습니다.

--- Assets/Script/YB_Script/Meat.cs
+++ Assets/Script/YB_Script/Meat.cs
@@ -8,6 +8,7 @@ using Random = UnityEngine.Random;
 public class Meat : MonoBehaviour
 {
   private SpriteRenderer _meatSprite;
+  public int meatLevel;
   private void Start()
   {
     StartCoroutine(Roast(Random.Range(0.5f, 1f))); //고기의 익는 시간을 랜덤하게 반환합니다.
@@ -19,8 +20,10 @@ public class Meat : MonoBehaviour
   IEnumerator Roast(float timer)  
   {
     yield return new WaitForSeconds(timer);
-    _meatSprite.DOColor(new Color(150f/255f, 75f/255f, 0), 1f); //고기가 익었을 시 갈색으로 변합니다.
+    _meatSprite.DOColor(new Color(150f/255f, 75f/255f, 0), 1f); //고기가 익었을 시 갈색으로 변합니다. 이 때 클릭 시 점수가  올라갑니다.
+    meatLevel = 1;
     yield return new WaitForSeconds(2f);
-    _meatSprite.DOColor(new Color(0, 0, 0), 1f);//고기가 탔을 시 갈색으로 변합니다. 이 때 점수가 깎입니다.
+    _meatSprite.DOColor(new Color(0, 0, 0), 1f);//고기가 탔을 시 갈색으로 변합니다. 이 때 클릭 시 점수가 깎입니다.
+    meatLevel = 2;
   }
 }
--- Assets/Script/YB_Script/Mouse.cs
+++ Assets/Script/YB_Script/Mouse.cs
@@ -1,8 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
-   
+
+    private void Update()
+    {
+        Click();
+    }
+
+    void Click()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 0f);
+            if (hit.collider.CompareTag("Meat"))
+            {
+                var meatLevel = hit.transform.gameObject.GetComponent<Meat>().meatLevel;
+                switch (meatLevel)
+                {
+                    case 0:
+                        Debug.Log("raw meat");
+                        break;
+                    case 1:
+                        Debug.Log("cooked meat");
+                        break;
+                    case 2:
+                        Debug.Log("burnt meat");
+                        break;
+                }
+            }
+        }
+    }
 }
