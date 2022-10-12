34e90539112da770ca8b0b35b228b477b5497e66
ChoiYuBin <choi82354@gmail.com>
2022-10-12 19:24:58 +0900

    Add annotations in MeatSpawner Script
    고기 스포너 스크립트에 주석을 달아주었습니다.

--- Assets/Script/YB_Script/MeatSpawner.cs
+++ Assets/Script/YB_Script/MeatSpawner.cs
@@ -9,24 +9,23 @@ using Random = UnityEngine.Random;
 
 public class MeatSpawner : MonoBehaviour
 {
-    public int meatCount;
-    [SerializeField] private float maxYPos;
-    [SerializeField] private float maxXPos;
-    [SerializeField] private GameObject meatObject;
-    [SerializeField] private TextMeshProUGUI meatCountText;
+    public int meatCount; // 현재 불판에 있는 고기의 개수를 담을 변수입니다.
+    [SerializeField] private float maxYPos; // 고기가 생성될 수 있는 위치에서 Y좌표의 최댓값을 담을 변수입니다.
+    [SerializeField] private float maxXPos; // 고기가 생성될 수 있는 위치에서 X좌표의 최댓값을 담을 변수입니다.
+    [SerializeField] private GameObject meatObject; // 고기 오브젝트입니다.
+    [SerializeField] private TextMeshProUGUI meatCountText; // 현재 불판에 있는 고기의 개수를 보여줄 Text입니다.
 
-    public void MeatSpawn()
+    public void MeatSpawn() // 버튼에 달아줄 함수입니다. 클릭 시 고기를 불판 위에 생성합니다.
     {
-        if (meatCount > 9) return;
-        meatCount++;
-        RefreshCountText();
-        Debug.Log(-maxXPos + "//" + -1 * maxXPos);
+        if (meatCount > 9) return;  // 고기가 10개 이상일 때에는 아래 코드를 실행하지 못하도록 return 해줍니다.
+        meatCount++; // 고기의 개수를 올려줍니다.
+        RefreshCountText(); // 위에서 고기의 개수를 하나 증가시켰으니 함수를 호출하여 Text를 새로고침 해줍니다.
         Instantiate(meatObject, 
-            new Vector2(Random.Range(-1 * maxXPos, maxXPos), Random.Range(-1 * maxYPos, maxYPos)),
-            quaternion.identity);
+            new Vector2(Random.Range(-maxXPos, maxXPos), Random.Range(-maxYPos, maxYPos)),
+            quaternion.identity); // 고기를 위에서 지정한 위치 안에서 랜덤한 부분에 생성합니다.
     }
 
-    public void RefreshCountText()
+    public void RefreshCountText() // 고기의 개수를 보여주는 Text를 새로고침 하는 용도입니다.
     {
         meatCountText.text = $"고기 수: {meatCount}";
     }
