7a0fd9d6887d875941297a11a62c5e42e9e7ccda
ChoiYuBin <choi82354@gmail.com>
2022-10-12 16:25:54 +0900

    Fix MeatSpawner Script
    고기의 갯수를 파악해줄 meatCount 스크립트를 수정하였고 UI로 보여줄 Text와 상호작용할 코드도 작성하였습니다.

--- Assets/Script/YB_Script/MeatSpawner.cs
+++ Assets/Script/YB_Script/MeatSpawner.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class MeatSpawner : MonoBehaviour
 {
-    private int _meatCount;
+    public int meatCount;
     [SerializeField] private float maxYPos;
     [SerializeField] private float maxXPos;
     [SerializeField] private GameObject meatObject;
+    [SerializeField] private Text meatCountText;
 
     public void MeatSpawn()
     {
-        if (_meatCount > 9) return;
+        if (meatCount > 9) return;
+        RefreshCountText();
         Debug.Log(-maxXPos + "//" + -1 * maxXPos);
         Instantiate(meatObject, 
             new Vector2(Random.Range(-1 * maxXPos, maxXPos), Random.Range(-1 * maxYPos, maxYPos)),
             quaternion.identity);
+    }
+
+    public void RefreshCountText()
+    {
+        meatCountText.text = $"고기 수: {meatCount}";
     }
 }
