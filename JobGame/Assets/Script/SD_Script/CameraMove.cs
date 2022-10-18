3e144318957aaab354cd05f0b0924b8bec2b8765
Simglachi <simda040416@gmail.com>
2022-10-18 16:34:30 +0900

    Fix CameraMove
    카메라 무브 스크립트를 좀 더 간단하게 완성 시켰습니다.

--- JobGame/Assets/Script/SD_Script/CameraMove.cs
+++ JobGame/Assets/Script/SD_Script/CameraMove.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {   
-    //부드럽게 이동될 감도
-    public float smoothTime = 10f;
-    
-    //이동할 타겟
-    public Vector3 target;
+    //이동할 좌표
+    public Vector3 RTarget;
+    public Vector3 LTarget;
 
-    //속도
-    private float xVelocity = 30f;
-    private float yVelocity = 0;
 
-    void Update() 
-    {   
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray,out hit))
+    void Update()
+    {
+        //D를 누르면 RTarget Position으로 이동됩니다.
+        if(Input.GetKeyDown(KeyCode.D))
         {
-            if(hit.transform.gameObject.tag == "Right")
-            {
-            Debug.Log("오른쪽 화면 클릭");
-            float newPositioX = Mathf.SmoothDamp(transform.position.x,target.x,ref xVelocity,smoothTime);        
-            float newPositioY = Mathf.SmoothDamp(transform.position.y,target.y,ref yVelocity,smoothTime);
-            transform.position = new Vector3(newPositioX,newPositioY,transform.position.z);
-            }
+            transform.position = RTarget;
+        }
+        //A를 누르면 LTarget Position으로 이동됩니다.
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = LTarget;
         }
     }
 }
