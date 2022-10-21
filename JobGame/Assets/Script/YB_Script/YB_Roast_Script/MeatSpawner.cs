00f436550c14a5549db77de725214146374ba423
ChoiYuBin <choi82354@gmail.com>
2022-10-21 18:55:54 +0900

    Remove YB_Script
    파일 구조를 변경하였습니다.

--- JobGame/Assets/Script/YB_Script/YB_Roast_Script/MeatSpawner.cs
+++ JobGame/Assets/Script/YB_Script/YB_Roast_Script/MeatSpawner.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class MeatSpawner : MonoBehaviour
