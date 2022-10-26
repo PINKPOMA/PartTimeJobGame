4d2202a42ef54f1d96cd9a67dcadb0449628d33b
Simglachi <simda040416@gmail.com>
2022-10-26 11:28:11 +0900

    Fix PotatoScript
    랜덤으로 생성될 위치를 정해주고, Potato 태그에 부딪히면 삭제됩니다.

--- JobGame/Assets/Script/SD_Script/Potato.cs
+++ JobGame/Assets/Script/SD_Script/Potato.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class Potato : MonoBehaviour
 {
     float distance = 10;
-   public void OnMouseDrag() 
-   {
+
+    //처음 감자 오브젝트는 랜덤으로 생성됩니다.
+    void Start()
+    {
+        this.transform.position = new Vector3(Random.Range(-3f,3f),2f,0);
+    }
+
+    public void OnMouseDrag() 
+    {
     //마우스를 드래그 하면 감자 오브젝트가 움직이는 스크립트 입니다.
         Vector3 mousePosition = new Vector3(Input.mousePosition.x,
                                         Input.mousePosition.y,distance);
         Vector3 PotatoPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = PotatoPosition;
-   }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Potato")
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
