60e81010f5484fade5b0285dd244841a366587b7
ChoiYuBin <choi82354@gmail.com>
2022-10-26 15:48:40 +0900

    Fix PlateSpawner Script
    그릇을 생성하는 스크립트를 완성하였습니다. 현재 클래스를 인스펙터에 보여주는 방식을 이용하여 2차 배열의 모습을 인스펙터에 띄웠는데 문제가 생기는 경우 고칠 예정입니다.

--- JobGame/Assets/Script/YB_Script/YB_TableClear_Script/PlateSpawner.cs
+++ JobGame/Assets/Script/YB_Script/YB_TableClear_Script/PlateSpawner.cs
@@ -1,27 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class PlateSpawner : MonoBehaviour
 {
     [Serializable]
     public class PlateArray
     {
-        [FormerlySerializedAs("_plate")] [SerializeField] private GameObject[] plate;
+        [FormerlySerializedAs("_plate")] public GameObject[] plate;
     }
-
     [FormerlySerializedAs("_plateArray")] [SerializeField]
-    private PlateArray[] plateArray; 
+    private PlateArray[] plateArray;
+    [SerializeField]private Transform[] platePos;
     void Start()
     {
-        
+        CreatePlate();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreatePlate()
     {
-        
+        Shuffle();
+        for (int i = 0; i < 4; i++)
+        {
+            Instantiate(plateArray[i].plate[Random.Range(0,4)], platePos[i].position,quaternion.identity);
+        }
+    }
+
+    void Shuffle()
+    {
+        for (int i = 0; i < plateArray.Length; ++i)
+        {
+            var random1 = Random.Range(0, plateArray.Length);
+            var random2 = Random.Range(0, plateArray.Length);
+
+            var temp = plateArray[random1];
+            plateArray[random1] = plateArray[random2];
+            plateArray[random2] = temp;
+        }
     }
 }
