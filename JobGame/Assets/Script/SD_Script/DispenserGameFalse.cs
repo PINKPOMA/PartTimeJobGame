20b6cf0d04cf22a0001874b744807b997410a268
Simglachi <simda040416@gmail.com>
2022-11-22 15:33:44 +0900

    Add DispenserGameFalse.Cs
    내용은 PotatoGameFalse와 비슷합니다.

---
+++ JobGame/Assets/Script/SD_Script/DispenserGameFalse.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispenserGameFalse : MonoBehaviour
+{
+    //PotatoGameFalse와 거의 동일한 스크립트 입니다.
+    float distance = 10f;
+    player PlayerCs;
+
+    void Awake()
+    {
+        PlayerCs = GameObject.Find("Player").GetComponent<player>();
+        //플레이어 스크립트 불러오기.
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,distance);
+        Vector3 PepsiPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = PepsiPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Tray")
+        {
+            PlayerCs.PepsiHave ++;
+            gameObject.SetActive(false);
+        }
+    }
+   
+}
