424d64cffe58ef8769bfb13f9b687ca505059811
Simglachi <simda040416@gmail.com>
2022-11-22 15:36:05 +0900

    Fix GameActive,Player.Cs
    사용하지 않은 변수 제거 및 약간의 추가와 수정을 하였습니다. 무한 게임 생성에 대한 조건문은 다시 작성 해야 할것 같습니다.

--- JobGame/Assets/Script/SD_Script/GameActive.cs
+++ JobGame/Assets/Script/SD_Script/GameActive.cs
@@ -16,32 +16,27 @@ public class GameActive : MonoBehaviour
     //무한 게임 생성 방지를 위한
     int Plimit = 1;
     int Dlimit = 2;
-    int Gamenum = 0;
-    int DGamenum = 0;
+    public int Gamenum = 0;
+    public int DGamenum = 0;
 
     //감자게임 오브젝트 스크립트
     PotatoGame potatogameCs;
-    GameObject potatogameObj;
-    public GameObject PotatoGameObj;
-
-    void Start()
-    {
-        potatogameObj = PotatoGameObj.transform.GetChild(PotatoGame);
-        potatogameCs = potatogameObj.GetComponent<PotatoGame>();
-
-    }
+    GameObject potatogame;
+    
     void OnMouseDown()
     {
         if(tag == "PotatoOven")
-        {
+        {  
+            //무한 게임 시작 방지 조건문
             Gamenum ++;
-            if(Gamenum == Plimit)
+            if(Gamenum == Plimit && !Potatoup.activeInHierarchy)
             {
                 Instantiate(PotatoGame);
             }
         }
         else if(tag == "Dispenser")
         {
+            //무한 게임 시작 방지 조건문
             DGamenum = +2;
             if(DGamenum == Dlimit && !Dispenserup.activeInHierarchy )
             {
@@ -67,14 +62,17 @@ public class GameActive : MonoBehaviour
             DGamenum = 0;
         }
     }
-
-    void FixedUpdate()
+    
+    public void PotatoReady()
     {
-        if(potatogameCs.PFlase == true)
-         {
-            Potatoup.SetActive(true);
-         }
+        Potatoup.SetActive(true);
     }
+
+    public void DispenserReady()
+    {
+        Dispenserup.SetActive(true);
+    }
+    
     IEnumerator DispenserG()
     {
         
--- JobGame/Assets/Script/SD_Script/player.cs
+++ JobGame/Assets/Script/SD_Script/player.cs
@@ -6,11 +6,10 @@ public class player : MonoBehaviour
 {
     //지니고 있는 감자의 갯수
     public int potatoHave;
-    public GameObject PotatoUp;
 
     //현재 지니고 있는 콜라의 갯수(Dispenser에서 생성)
-    public int ColaHave;
-    public GameObject ColaUp;
+    public int PepsiHave;
+  
 
 
 }
